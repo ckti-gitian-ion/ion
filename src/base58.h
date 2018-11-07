d547f2c352aa226f2e36e828e5fe46ebe817de72
FornaxA <fornaxa@servitising.org>
2018-11-07 11:41:01 +0100

    IONCORE-47 Introduction of wrappers

--- src/base58.h
+++ src/base58.h
@@ -172,10 +172,9 @@ public:
 typedef CBitcoinExtKeyBase<CExtKey, 74, CChainParams::EXT_SECRET_KEY> CBitcoinExtKey;
 typedef CBitcoinExtKeyBase<CExtPubKey, 74, CChainParams::EXT_PUBLIC_KEY> CBitcoinExtPubKey;
 
-/// Encode an old-style bitcoin address
-std::string EncodeLegacyAddr(const CTxDestination &dest, const CChainParams &);
-
-/// Decode an old-style bitcoin address
-CTxDestination DecodeLegacyAddr(const std::string &str, const CChainParams &);
+std::string EncodeDestination(const CTxDestination& dest);
+CTxDestination DecodeDestination(const std::string& str);
+bool IsValidDestinationString(const std::string& str);
+bool IsValidDestinationString(const std::string& str, const CChainParams& params);
 
 #endif // BITCOIN_BASE58_H
